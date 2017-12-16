e3cfd0e33cdae8c5620da1148e2589330e169bbc
smarly <smarly@smarly.net>
2017-12-16 17:39:17 +0300

    simple route example

--- Master.MVC/Global.asax.cs
+++ Master.MVC/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
@@ -9,16 +10,11 @@ namespace Master.MVC
 {
     public class Global : System.Web.HttpApplication
     {
-        protected void Application_BeginRequest(object sender, EventArgs e)
+        protected void Application_Start()
         {
-            HttpContext context = base.Context;
-            var request = context.Request;
-            var response = context.Response;
-
-            var url = request.Url.PathAndQuery;
-
-            response.Write($"Your request was '{url}'");
-            base.CompleteRequest();
+            RouteTable.Routes.MapPageRoute("HelloPageRoute",
+                                           "Hello",
+                                           "~/Pages/Hello.aspx");
         }
     }
 }
