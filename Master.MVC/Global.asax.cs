3bdbe5a835fa15e5bedb1617e6349aebfd2183c3
smarly <smarly@smarly.net>
2017-12-16 15:11:59 +0300

    Simple text response

---
+++ Master.MVC/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Master.MVC
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext context = base.Context;
+            var request = context.Request;
+            var response = context.Response;
+
+            var url = request.Url.PathAndQuery;
+
+            response.Write($"Your request was '{url}'");
+            base.CompleteRequest();
+        }
+    }
+}
