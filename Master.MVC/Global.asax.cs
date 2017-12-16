45afdc96312447a500bc8b05e90bc00521f32d38
smarly <smarly@smarly.net>
2017-12-16 17:53:03 +0300

    simple controller + action

--- Master.MVC/Global.asax.cs
+++ Master.MVC/Global.asax.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 
 namespace Master.MVC
 {
@@ -12,9 +7,13 @@ namespace Master.MVC
     {
         protected void Application_Start()
         {
-            RouteTable.Routes.MapPageRoute("HelloPageRoute",
-                                           "Hello",
-                                           "~/Pages/Hello.aspx");
+            //Route r = new Route(null, (IRouteHandler)new MvcRouteHandler());
+
+            RouteTable.Routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
