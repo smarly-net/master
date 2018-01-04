9392b00888cee10b2bd3819b329e3d95c7555944
denis.dmitriev <d.dzmitryieu@godeltech.com>
2018-01-04 18:00:28 +0300

    Sample route handler

--- Master.MVC/Global.asax.cs
+++ Master.MVC/Global.asax.cs
@@ -1,4 +1,4 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
 using System.Web.Routing;
 
 namespace Master.MVC
@@ -7,11 +7,36 @@ namespace Master.MVC
     {
         protected void Application_Start()
         {
-            RouteTable.Routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            RouteTable.Routes.Add("MyRoute", new Route("{*blam}", new SampleRouteHandler()));
         }
+    }
+
+    public class SampleRouteHandler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return new SampleHttpHandler(requestContext.RouteData);
+        }
+    }
+
+    public class SampleHttpHandler : IHttpHandler
+    {
+        private readonly RouteData _routeData;
+
+        public SampleHttpHandler(RouteData routeData)
+        {
+            _routeData = routeData;
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var val = _routeData.Values["blam"];
+
+            var response = context.Response;
+
+            response.Write($"Your request was '{val}'");
+        }
+
+        public bool IsReusable { get; }
     }
 }
