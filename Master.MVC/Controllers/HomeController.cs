c82c2d194e22ba4fda2c314c71ce60e4aa0c7376
smarly <smarly@smarly.net>
2018-01-19 06:04:36 +0300

    RouteAttribute

--- Master.MVC/Controllers/HomeController.cs
+++ Master.MVC/Controllers/HomeController.cs
@@ -2,11 +2,20 @@
 
 namespace Master.MVC.Controllers
 {
+    [RoutePrefix("Hello")]
     public class HomeController : Controller
     {
+        [Route("~/")]
+        [Route("home")]
+        [Route("")]
         public string Index()
         {
             return "Hello MVC";
+        }
+        [Route("about")]
+        public string About()
+        {
+            return "About MVC";
         }
     }
 }
--- Master.MVC/Global.asax.cs
+++ Master.MVC/Global.asax.cs
@@ -10,47 +10,7 @@ namespace Master.MVC
     {
         protected void Application_Start()
         {
-            ControllerBuilder.Current.SetControllerFactory(new DefaultControllerFactory1());
-
-//            var f = new DefaultControllerFactory();
-
-            RouteTable.Routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}",
-                defaults: new { controller = "Home", action = "Index" }
-            );
-        }
-    }
-
-    public class DefaultControllerFactory1 : DefaultControllerFactory
-    {
-        public override IController CreateController(RequestContext requestContext, string controllerName)
-        {
-            var controller = base.CreateController(requestContext, controllerName);
-            return controller;
-        }
-
-        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
-        {
-            var controllerInstance = base.GetControllerInstance(requestContext, controllerType);
-            return controllerInstance;
-        }
-
-        protected override SessionStateBehavior GetControllerSessionBehavior(RequestContext requestContext, Type controllerType)
-        {
-            var controllerSessionBehavior = base.GetControllerSessionBehavior(requestContext, controllerType);
-            return controllerSessionBehavior;
-        }
-
-        protected override Type GetControllerType(RequestContext requestContext, string controllerName)
-        {
-            var controllerType = base.GetControllerType(requestContext, controllerName);
-            return controllerType;
-        }
-
-        public override void ReleaseController(IController controller)
-        {
-            base.ReleaseController(controller);
+            RouteTable.Routes.MapMvcAttributeRoutes();
         }
     }
 }
