61740634075ef8496f3842f986b4d2fb858eb7bb
smarly <smarly@smarly.net>
2018-01-22 23:07:42 +0300

    Contoller filters

--- Master.MVC/Controllers/HomeController.cs
+++ Master.MVC/Controllers/HomeController.cs
@@ -2,18 +2,73 @@
 using System.Diagnostics;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Filters;
 using System.Web.Routing;
 
 namespace Master.MVC.Controllers
 {
-    public class HomeController : IController
+    public class HomeController : Controller
     {
-        public void Execute(RequestContext requestContext)
+        public ActionResult Index(string id)
         {
-            string controller = (string)requestContext.RouteData.Values["controller"];
-            string action = (string)requestContext.RouteData.Values["action"];
+            if (id == "none")
+            {
+                throw new NotImplementedException();
+            }
 
-            requestContext.HttpContext.Response.Write($"Controller: {controller}, Action: {action}");
+            return this.View();
         }
+
+        #region Overrides of Controller
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Debug.WriteLine("IActionFilter::OnActionExecuting");
+            base.OnActionExecuting(filterContext);
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Debug.WriteLine("IActionFilter::OnActionExecuted");
+            base.OnActionExecuted(filterContext);
+        }
+
+        protected override void OnAuthentication(AuthenticationContext filterContext)
+        {
+            Debug.WriteLine("IAuthenticationFilter::OnAuthentication");
+            base.OnAuthentication(filterContext);
+        }
+
+        protected override void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            Debug.WriteLine("IAuthenticationFilter::OnAuthenticationChallenge");
+            base.OnAuthenticationChallenge(filterContext);
+        }
+
+        protected override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            Debug.WriteLine("IAuthorizationFilter::OnAuthorization");
+            base.OnAuthorization(filterContext);
+        }
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            Debug.WriteLine("IExceptionFilter::OnException");
+            base.OnException(filterContext);
+        }
+
+        protected override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Debug.WriteLine("IResultFilter::OnResultExecuted");
+            base.OnResultExecuted(filterContext);
+        }
+
+        protected override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Debug.WriteLine("IResultFilter::OnResultExecuting");
+            base.OnResultExecuting(filterContext);
+        }
+
+        #endregion
     }
 }
