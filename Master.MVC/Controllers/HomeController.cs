23dd40e71f74b93e16ca6ac83925c88577d4b095
smarly <smarly@smarly.net>
2018-01-23 00:19:56 +0300

    Custom model binding

--- Master.MVC/Controllers/HomeController.cs
+++ Master.MVC/Controllers/HomeController.cs
@@ -9,6 +9,48 @@ using System.Web.Routing;
 
 namespace Master.MVC.Controllers
 {
+    //[ModelBinder(typeof(ByRegionBinder))]
+    public class ByRegion
+    {
+        public ByRegion(string region)
+        {
+            Region = region;
+        }
+
+        public string Region { get; private set; }
+    }
+
+    public class ByRegionBinder : IModelBinder {
+        #region Implementation of IModelBinder
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var code = controllerContext.HttpContext.Request.Form[bindingContext.ModelName];
+
+            switch (code)
+            {
+                case "1":
+                    return new ByRegion("Брестская область");
+                case "2":
+                    return new ByRegion("Витебская область");
+                case "3":
+                    return new ByRegion("Гомельская область");
+                case "4":
+                    return new ByRegion("Гродненская область");
+                case "5":
+                    return new ByRegion("Минская область");
+                case "6":
+                    return new ByRegion("Могилёвская область");
+                case "7":
+                    return new ByRegion("г. Минск");
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+    }
+
     public class HomeController : Controller
     {
         public ActionResult Index(string id)
@@ -17,9 +59,9 @@ namespace Master.MVC.Controllers
         }
 
         [HttpPost]
-        public string DecodeBase64(Binary binary)
+        public string DecodeBase64(/*[ModelBinder(typeof(ByRegionBinder))]*/ByRegion region)
         {
-            return Encoding.UTF8.GetString(binary.ToArray());
+            return region.Region;
         }
     }
 }
